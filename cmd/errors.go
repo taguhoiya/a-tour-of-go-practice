e351bce4d75e8c31c56573c41a63a22dd294ba83
Takudai Kawasaki <ouch.taguhoiya@outlook.jp>
2022-06-06 03:01:02 +0900

    progress by 2022/06/06

--- cmd/errors.go
+++ cmd/errors.go
@@ -3,12 +3,27 @@ package main
 import (
 	"fmt"
 	"time"
+	"reflect"
 )
 
 type MyError struct {
-	
+	When time.Time
+	What string
 }
 
-func main  () {
+func (e *MyError) Error() string {
+	return fmt.Sprintf("at %v, %s", e.When, e.What)
+}
 
+func run() error {
+	return &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+}
+
+func main () {
+	if err := run(); err != nil {
+		fmt.Println(reflect.TypeOf(err))
+	}
 }
